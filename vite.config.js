406c6f6efb32aa7c698d88d51c43e4533d7c3b7f
Chethan P <cp187.2006@gmail.com>
2026-07-23 15:55:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
 export default defineConfig({
+  base: '/ROCKET-NOVA-AI-for-real-time-satellite-health-monitoring-and-collision-prevention/',
   plugins: [react()],
   resolve: {
     alias: {
